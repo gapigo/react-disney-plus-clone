44020170219df01ad2edfe992a0afe491d01d165
gapigo <x.gabriel12@gmail.com>
2022-01-30 01:36:13 -0300

    Adding assets + Evolving the navbar

--- disney-plus-clone/src/components/Header.js
+++ disney-plus-clone/src/components/Header.js
@@ -4,14 +4,83 @@ import styled from 'styled-components';
 
 function Header() {
     return (
-        <Container>
-            Header
-        </Container>
+        <Nav>
+            <Logo src="/images/logo.svg"/>
+            <NavMenu>
+                <a>
+                    <img src="/images/home-icon.svg"/>
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg"/>
+                    <span>SEARCH</span>
+                </a>
+                <a>
+                    <img src="/images/watchlist-icon.svg"/>
+                    <span>WATCHLIST</span>
+                </a>
+                <a>
+                    <img src="/images/original-icon.svg"/>
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg"/>
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg"/>
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImg src="https://yt3.ggpht.com/yti/APfAmoF5xYCImlVxwgs5mQq4AK2FN5J5kPw1jbRyg4QUekQ=s88-c-k-c0x00ffffff-no-rj-mo" />
+        </Nav>
     );
 }
 
 export default Header;
-const Container = styled.div`
+const Nav = styled.nav`
     height: 70px;
     background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`;
+
+const Logo = styled.img`
+    width: 80px;
+`;
+
+const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px;
+    align-items: center;
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+
+            &:after {
+                content: "Hello";
+                height: 2px;
+                background: white;
+            }
+        }
+    }
+`;
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
+
 `;
