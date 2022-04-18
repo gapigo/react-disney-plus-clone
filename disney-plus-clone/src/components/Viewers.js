7ed8e965d830751dfea1e1d7370e55fc1789c3b7
gapigo <x.gabriel12@gmail.com>
2022-04-17 23:37:57 -0300

    Completing the carousel and astarting the Viewers

---
+++ disney-plus-clone/src/components/Viewers.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Viewers() {
+  return (
+    <Container>
+        Viewers
+    </Container>
+  )
+}
+
+export default Viewers
+
+const Container = styled.div`
+    margin-top: 30px;
+    display: grid;
+    grid-template: columns;
+
+`
