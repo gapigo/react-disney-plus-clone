b10871e34416588e69dd88de42e5e690f50b2c4b
gapigo <x.gabriel12@gmail.com>
2022-04-24 04:10:46 -0300

    Adding Login page and few minor fixes in Detail page

--- disney-plus-clone/src/components/Detail.js
+++ disney-plus-clone/src/components/Detail.js
@@ -65,6 +65,7 @@ const ImageTitle = styled.div`
     width: 35vw;
     min-height: 170px;
     min-width: 200px;
+    margin-top: 60px;
 
     img {
         height: 100%;
@@ -136,4 +137,5 @@ const Description = styled.div`
     font-size: 20px;
     margin-top: 16px;
     color: rgb(249, 249, 249);   
+    max-width: 760px;
 `
